762e92f76c3cc887a6fdd21d0dccaed5f154e438
Rob <robert.bell@rxpservices.com>
2022-01-27 14:26:43 +1100

    Add wordle helper post

--- components/layout.js
+++ components/layout.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
-import headShot from '../public/images/doesnotexist.jpg?resize&size=300'
 
-const name = 'Your Name'
+const name = 'Random notes and musings'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -28,30 +27,10 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <img
-              priority
-              src={headShot.src}
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
-            <Link href="/">
-              <a>
-                <img
-                  priority
-                  src={headShot.src}
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
                 <a className={utilStyles.colorInherit}>{name}</a>
